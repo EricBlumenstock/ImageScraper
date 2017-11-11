a487a056dea2efc22f95871681df8d1445ef9f72
Eric Blumenstock <ericblumenstock@gmail.com>
2017-11-11 14:26:16 -0500

    Rough idea

--- Scrape.py
+++ Scrape.py
@@ -1,20 +1,28 @@
 import selenium.webdriver as webdriver
 import selenium.webdriver.support.ui as ui
 import urllib.request as urllib
-
+import time
 
 def main():
 
-    wd = webdriver.Chrome()
+    wdo = webdriver.ChromeOptions()
+    #prefs = {"profile.managed_default_content_settings.images": 2}
+    #wdo.add_experimental_option("prefs", prefs)
+    #https://clients2.google.com/service/update2/crx?response=redirect&prodversion=60&x=id%3Dcjpalhdlnbpafiamejdnhcphjbkeiagm%26uc
+    wdo.add_extension('C:\Webdrivers\extension_1_14_18.crx')
 
+    wd = webdriver.Chrome(chrome_options = wdo)
+
+    wd.implicitly_wait(5)
     wd.get("http://kissmanga.com/Manga/Superior-Cross/Vol-001-Ch-001-Read-Online?id=123252#1")
     wd.find_element_by_xpath('//*[@id="selectReadType"]/option[2]').click()
-    img = wd.find_elements_by_xpath('//*[@id="divImage"]//img[@src]')
-    wd.close()
+    img = wd.find_elements_by_xpath('//*[@id="divImage"]//img')
 
+    c=0
     for i in img:
-        urllib.urlretrieve(img, i + '.png')
-
+        urllib.urlretrieve(i.get_attribute('src'), str(c) + '.png')
+        c=c+1
+    wd.close()
     urllib.urlcleanup()
 
 
