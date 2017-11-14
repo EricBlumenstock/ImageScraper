050970a5ec407828c0baaae31aeb9df87935f4dc
Eric Blumenstock <ericblumenstock@gmail.com>
2017-11-14 07:17:23 -0500

    Rough draft revisited

--- Scrape.py
+++ Scrape.py
@@ -4,11 +4,8 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait as WDW
 import urllib.request as urllib
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 import os
 import time
-import timeit
 
 
 #Names files 0.png, 1.png, ...
@@ -46,9 +43,9 @@ def main():
     IMGGROUPS = '.listing a'
 
     wdo = webdriver.ChromeOptions()
-    wdo.add_extension('C:\Webdrivers\extension_1_14_18.crx')
-    #wdo.add_argument('--headless')
-    #wdo.add_argument('--disable-gpu')
+    #wdo.add_extension('C:\Webdrivers\extension_1_14_18.crx')
+    wdo.add_argument('--headless')
+    wdo.add_argument('--disable-gpu')
 
     capa = DC.CHROME
     capa["pageLoadStrategy"] = "none"
@@ -56,44 +53,36 @@ def main():
     #prefs = {"profile.managed_default_content_settings.images": 2}
     #wdo.add_experimental_option("prefs", prefs)
     #https://clients2.google.com/service/update2/crx?response=redirect&prodversion=60&x=id%3Dcjpalhdlnbpafiamejdnhcphjbkeiagm%26uc
+
     wd = webdriver.Chrome(chrome_options=wdo)
     wait = WDW(wd, 20)
-
-    start = timeit.default_timer()
 
     wd.get(START)
     time.sleep(6) #wait a moment for javascript to give us access to website
 
     groups = wd.find_elements_by_css_selector(IMGGROUPS)
-
-    # BEGIN THREAD
-
     groups = to_attribute_list(groups, 'href')
-
 
     for g in reversed(groups):
         wd.execute_script('''window.open("''' + g + '''","_blank");''')
-        time.sleep(1)
+        time.sleep(2)
         wd.switch_to.window(wd.window_handles[1])
 
         wait.until(EC.presence_of_element_located((By.XPATH, ALLDROPDOWN))) #click ASAP
         wd.find_element_by_xpath(ALLDROPDOWN).click() #click the ALL images selection
-        time.sleep(4)
+
         wait.until(EC.presence_of_all_elements_located((By.XPATH, ACTUALIMAGES))) #find all ASAP
         imgs = wd.find_elements_by_xpath(ACTUALIMAGES)  # find all of the images we want
 
         wd.execute_script("window.stop();") #stop loading the page
 
-        #download_images(to_attribute_list(imgs, 'src'), create_directory())
+        download_images(to_attribute_list(imgs, 'src'), create_directory())
 
         #Close tab
         wd.execute_script('''window.close();''')
         wd.switch_to.window(wd.window_handles[0])
-        #body = wd.find_element(By.TAG_NAME, 'body')
-        #body.send_keys(Keys.CONTROL, 'w')
 
     wd.quit() #Unfortunately this has to remain open for the session to work
 
-    print(timeit.default_timer() - start)#12
 
 main()
